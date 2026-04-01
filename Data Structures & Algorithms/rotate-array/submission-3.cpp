a729835f138d590f78b308b4cac13929c18379ce
Rupesh Omprakash Honrao <149290951+Rupesh5988@users.noreply.github.com>
2026-04-01 09:19:00 +0530

    Add: rotate-array - submission-3

---
+++ Data Structures & Algorithms/rotate-array/submission-3.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        if(k==0) return;
+        else if(k>nums.size()) k%=nums.size();
+
+        int i=0,j=nums.size()-k-1;
+        while(i<=j){
+            swap(nums[i++],nums[j--]);
+        }
+        i=nums.size()-k,j=nums.size()-1;
+        while(i<=j){
+            swap(nums[i++],nums[j--]);
+        }
+        i=0,j=nums.size()-1;
+        while(i<=j){
+            swap(nums[i++],nums[j--]);
+        }
+    }
+};
