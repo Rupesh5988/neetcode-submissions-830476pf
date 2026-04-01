874fa6fa5794e401328a9c1977699f12a304e8a0
Rupesh Omprakash Honrao <149290951+Rupesh5988@users.noreply.github.com>
2026-04-01 09:48:00 +0530

    Add: boats-to-save-people - submission-0

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(),people.end());
+        int ans=0,lo=0,hi=people.size()-1;
+        while(lo<=hi){
+            if(people[lo]+people[hi]<=limit) {ans++; lo++; hi--;}
+            else{ans++; hi--;}
+        }
+        return ans;
+    }
+};
