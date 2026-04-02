b0471548e3315b8b77124672e8a67b14fb54e225
Rupesh Omprakash Honrao <149290951+Rupesh5988@users.noreply.github.com>
2026-04-02 10:44:24 +0530

    Add: contains-duplicate-ii - submission-4

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-4.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        if(nums.size()==0 or nums.size()==1) return false;
+        int i=0;
+        bool check=false;
+        while(i<nums.size()){
+            int j=i+1;
+            while(j<nums.size()){
+                if(nums[j]==nums[i]){if(abs(i-j)<=k) { j++;check=true;}
+                else {check=false; j++;}
+                }
+                else j++;
+            }
+            i++;
+        }
+        if(check==false) return false;
+        return true;
+    }
+};
