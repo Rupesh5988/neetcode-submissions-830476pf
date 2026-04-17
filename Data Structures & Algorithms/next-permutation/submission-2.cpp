2e600f3588455405bae67074f6bcbab1259733cd
Rupesh Omprakash Honrao <149290951+Rupesh5988@users.noreply.github.com>
2026-04-17 07:27:04 +0530

    Add: next-permutation - submission-2

---
+++ Data Structures & Algorithms/next-permutation/submission-2.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int idx = -1;
+        int n = nums.size();
+
+     
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                idx = i;
+                break; 
+            }
+        }
+
+      
+        if (idx == -1) {
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+     reverse(nums.begin() + idx + 1, nums.end());
+       int k=-1;
+        for (int i = idx;i<n;i++) {
+            if (nums[i] > nums[idx]) {
+                k=i;
+                break;
+            }
+        }
+        swap(nums[idx],nums[k]);
+
+       
+    }
+};
